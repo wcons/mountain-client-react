fbe78fa982b483d30e7989c48214a417cf99f9e4
wcons <wconstantino@gmail.com>
2019-07-24 15:25:58 -0400

    Add Create form and functionality
      -Installed Leaflet component for potential future use
      -Some light CSS styling and image

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,6 +6,7 @@ import './Header.scss'
 const authenticatedOptions = (
   <React.Fragment>
     <Link to="/mountains">Mountains</Link>
+    <Link to="/list-mountain">List a Mountain</Link>
     <Link to="/change-password">Change Password</Link>
     <Link to="/sign-out">Sign Out</Link>
   </React.Fragment>
