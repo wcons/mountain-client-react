1a92adff62bb7bbfe765490ed6c16dd9d1442b81
wcons <wconstantino@gmail.com>
2019-07-25 20:27:42 -0400

    Changed production apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://lit-wildwood-72208.herokuapp.com',
+  production: 'https://evening-taiga-11840.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
